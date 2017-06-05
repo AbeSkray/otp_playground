bc5d4a3f71ce08960ec490707ef592459c39133b
Alexander Knowles <alexander.knowles@gmail.com>
2017-06-05 08:49:15 -0500

    Add FridgeServer process to a supervision tree.
    We can interact with it using the variable 'Fridge' in iex.

--- lib/otp_playground/application.ex
+++ lib/otp_playground/application.ex
@@ -10,8 +10,7 @@ defmodule OtpPlayground.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: OtpPlayground.Worker.start_link(arg1, arg2, arg3)
-      # worker(OtpPlayground.Worker, [arg1, arg2, arg3]),
+      worker(OtpPlayground.FridgeServer, [[name: Fridge]]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
